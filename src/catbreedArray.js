fd6d699f8c078fd667685ef94cc1c9031d886493
idaism <ida.bjarney@outlook.com>
2022-10-31 17:21:18 +0000

    html and css for catbreed selector

---
+++ src/catbreedArray.js
@@ -0,0 +1,193 @@
+
+
+export default [
+  {
+    name: 'Abyssinian Cat',
+    size: ['small', 'medium'],
+    coat: 'medium',
+    energy: 'high',
+    shedding: 'minimal',
+  },
+  {
+    name: 'American Bobtail Cat',
+    size: ['medium', 'large'],
+    coat: ['short', 'medium', 'long'],
+    energy: 'high',
+    shedding: 'minimal'
+  },
+  {
+    name: 'American Curl Cat',
+    size: ['small', 'medium'],
+    coat: ['short', 'long'],
+    energy: 'high',
+    shedding: 'minimal'
+  },
+  {
+    name: 'American Shorthair Cat',
+    size: ['medium', 'large'],
+    coat: 'short',
+    energy: 'medium',
+    shedding: 'minimal'
+  },
+  {
+    name: 'American Wirehair Cat',
+    size: 'Balinese-Javanese Cat',
+    coat: 'medium',
+    energy: 'medium',
+    shedding: 'medium'
+  },
+  {
+    name: 'Balinese-Javanese Cat',
+    size: 'medium',
+    coat: 'medium',
+    energy: 'high',
+    shedding: 'moderate'
+  },
+  {
+    name: 'Bengal Cat',
+    size: ['medium', 'large'],
+    coat: 'medium',
+    energy: 'high',
+    shedding: 'minimal'
+  },
+  {
+    name: 'Birman Cat',
+    size: ['medium', 'large'],
+    coat: ['medium', 'long'],
+    energy: 'low',
+    shedding: 'moderate'
+  },
+  {
+    name: 'Bombay Cat',
+    size: 'medium',
+    coat: 'short',
+    energy: 'high',
+    shedding: 'minimal'
+  },
+  {
+    name: 'British Shorthair Cat',
+    size: ['medium', 'large'],
+    coat: 'short',
+    energy: 'low',
+    shedding: 'moderate'
+  },
+  {
+    name: 'Burmese Cat',
+    size: 'medium',
+    coat: 'short',
+    energy: '',
+    shedding: 'minimal'
+  },
+  {
+    name: 'Chartreux Cat',
+    size: ['medium', 'large'],
+    coat: 'medium',
+    energy: 'medium',
+    shedding: 'minimal'
+  },
+  {
+    name: 'Cornish Rex Cat',
+    size: ['small', 'medium'],
+    coat: 'short',
+    energy: 'high',
+    shedding: 'none'
+  },
+  {
+    name: 'Devon Rex Cat',
+    size: ['small', 'medium'],
+    coat: 'short',
+    energy: 'high',
+    shedding: 'minimal'
+  },
+  {
+    name: 'Egyptian Mau Cat',
+    size: 'medium',
+    coat: ['short', 'medium'],
+    energy: 'high',
+    shedding: 'moderate'
+  },
+  {
+    name: 'European Burmese Cat',
+    size: 'medium',
+    coat: 'short',
+    energy: 'high',
+    shedding: 'minimal'
+  },
+  {
+    name: 'Exotic Shorthair Cat',
+    size: ['medium', 'large'],
+    coat: 'short',
+    energy: 'low',
+    shedding: 'moderate'
+  },
+  {
+    name: 'Havana Brown Cat',
+    size: 'medium',
+    coat: ['short', 'medium'],
+    energy: 'medium',
+    shedding: 'minimal'
+  },
+  {
+    name: 'Himalayan Cat',
+    size: ['medium', 'large'],
+    coat: 'long',
+    energy: 'low',
+    shedding: 'heavy'
+  },
+  {
+    name: 'Japanese Bobtail Cat',
+    size: 'medium',
+    coat: 'medium',
+    energy: 'high',
+    shedding: 'moderate'
+  },
+  {
+    name: 'Korat Cat',
+    size: 'medium',
+    coat: 'short',
+    energy: 'high',
+    shedding: 'minimal'
+  },
+  {
+    name: 'LaPerm Cat',
+    size: 'medium',
+    coat: ['short', 'long'],
+    energy: 'medium',
+    shedding: 'minimal'
+  },
+  {
+    name: 'Maine Coon Cat',
+    size: 'large',
+    coat: 'long',
+    energy: 'medium',
+    shedding: 'heavy'
+  },
+  {
+    name: 'Manx Cat',
+    size: ['medium', 'large'],
+    coat: 'long',
+    energy: 'medium',
+    shedding: ['moderate', 'heavy']
+  },
+  {
+    name: 'Munchkin Cat',
+    size: 'small',
+    coat: ['long', 'short'],
+    energy: 'medium',
+    shedding: ['moderate', 'heavy']
+  },
+  {
+    name: 'Norwegian Forest Cat',
+    size: 'large',
+    coat: 'long',
+    energy: 'medium',
+    shedding: ['heavy', 'moderate']
+  },
+  {
+    name: 'Ocicat Cat',
+    size: ['medium', 'large'],
+    coat: 'short',
+    energy: 'high',
+    shedding: 'moderate'
+  }
+]
---
+++ src/script.js
@@ -0,0 +1,18 @@
+import catbreeds from './catbreedArray';
+
+
+
+const form = document.querySelector('.form');
+const resultsContainer = document.querySelector('.results-container')
+
+
+
+function getCatInfo() {
+  let cat = catbreeds;
+  console.log(cat)
+  resultsContainer.innerHTML = `
+  `
+}
+
+getCatInfo()
+
